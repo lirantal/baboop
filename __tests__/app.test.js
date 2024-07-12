826e81f039e316137e821a70d8b00092a033af1e
Liran Tal <liran.tal@gmail.com>
2024-07-12 15:52:56 +0300

    test: fix literal in callback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,73 @@
 import { test } from 'node:test'
 import assert from 'node:assert'
-import { add } from '../src/main.js'
+import { runCommandAndNotify } from '../src/main.js'
+import ChildProcess from 'node:child_process'
 
-test('1 + 1 is 2', () => {
-  assert.strictEqual(add(1, 1), 2)
+test('CLI doesnt receive any arguments to run and throws an error', async (t) => {
+  await assert.rejects(async () => await runCommandAndNotify(), {
+    message: 'Please provide a command to run.',
+  })
+})
+
+test('CLI program runs and completes successfully', async (t) => {
+  const mockedExitCode = 0
+  t.mock.method(ChildProcess, 'execFile', (cmd, args, callback) => {
+    callback(null, 'stdout', 'stderr')
+  })
+  const spy = t.mock.method(ChildProcess, 'spawn', (cmd) => {
+    return {
+      on: (event, callback) => {
+        if (event === 'exit') {
+          callback(mockedExitCode)
+        }
+      },
+    }
+  })
+
+  const commandToRun = 'ls'
+  const result = await runCommandAndNotify(commandToRun)
+  assert.strictEqual(spy.mock.calls.length, 1)
+  assert.strictEqual(result.program.code, 0)
+})
+
+test('CLI program runs and completes with a failure', async (t) => {
+  const mockedExitCode = 1
+  t.mock.method(ChildProcess, 'execFile', (cmd, args, callback) => {
+    callback(null, 'stdout', 'stderr')
+  })
+  const spy = t.mock.method(ChildProcess, 'spawn', (cmd) => {
+    return {
+      on: (event, callback) => {
+        if (event === 'exit') {
+          callback(mockedExitCode)
+        }
+      },
+    }
+  })
+
+  const commandToRun = 'ls'
+  const result = await runCommandAndNotify(commandToRun)
+  assert.strictEqual(spy.mock.calls.length, 1)
+  assert.strictEqual(result.program.code, 1)
+})
+
+test('CLI program runs and completes successfully and uses a long program name', async (t) => {
+  const mockedExitCode = 0
+  t.mock.method(ChildProcess, 'execFile', (cmd, args, callback) => {
+    callback(null, 'stdout', 'stderr')
+  })
+  const spy = t.mock.method(ChildProcess, 'spawn', (cmd) => {
+    return {
+      on: (event, callback) => {
+        if (event === 'exit') {
+          callback(mockedExitCode)
+        }
+      },
+    }
+  })
+
+  const commandToRun = 'npm run lint:fix'
+  const result = await runCommandAndNotify(commandToRun)
+  assert.strictEqual(spy.mock.calls.length, 1)
+  assert.strictEqual(result.program.code, 0)
 })
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { promisify } from 'node:util'
-import { spawn, execFile } from 'node:child_process'
-const execFilePromise = promisify(execFile)
+import ChildProcess from 'node:child_process'
 
 export async function runCommandAndNotify (commandToRun) {
   return new Promise((resolve, reject) => {
@@ -8,7 +6,7 @@ export async function runCommandAndNotify (commandToRun) {
       return reject(new Error('Please provide a command to run.'))
     }
 
-    const child = spawn(commandToRun, { shell: true, stdio: 'inherit' })
+    const child = ChildProcess.spawn(commandToRun, { shell: true, stdio: 'inherit' })
 
     child.on('exit', async (code) => {
       try {
@@ -19,17 +17,21 @@ export async function runCommandAndNotify (commandToRun) {
         const notificationCommand = 'osascript'
         const notificationCommandArguments = ['-e', `display notification "Finished ${exitStatus}" with title "${programName}"`]
 
-        const { stdout, stderr } = await execFilePromise(notificationCommand, notificationCommandArguments)
+        ChildProcess.execFile(notificationCommand, notificationCommandArguments, (error, stdout, stderr) => {
+          if (error) {
+            return reject(error)
+          }
 
-        return resolve({
-          notification: {
-            stdout,
-            stderr,
-          },
-          program: {
-            name: programName,
-            code,
-          },
+          return resolve({
+            notification: {
+              stdout,
+              stderr,
+            },
+            program: {
+              name: programName,
+              code,
+            },
+          })
         })
       } catch (error) {
         return reject(error)
