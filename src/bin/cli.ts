4c330c4606b672ca5cfb04586298eb18ab69712f
Liran Tal <liran.tal@gmail.com>
2025-01-19 14:50:11 +0200

    chore: ignore command injection security issue

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -5,6 +5,7 @@ const debug = debuglog('baboop')
 
 async function init () {
   const commandToRun = process.argv.slice(2).join(' ')
+  // file deepcode ignore IndirectCommandInjection: <accepted user input for the CLI is part of how this program works>
   const result = await runCommandAndNotify(commandToRun)
   debug(result.toString())
 }
